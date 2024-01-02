bbd81b9ad67470d75d3ec46c4bdbee55a5c4481a
kronyer <pedromila2000@gmail.com>
2024-01-02 11:21:13 -0300

    Classe posiçao

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Chess_Curso
 {
@@ -10,6 +7,10 @@ namespace Chess_Curso
     {
         static void Main(string[] args)
         {
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
         }
     }
 }
