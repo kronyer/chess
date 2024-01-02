cd4ab0602252968745ebfd7989e96f7dea45c834
kronyer <pedromila2000@gmail.com>
2024-01-02 12:22:29 -0300

    conversão da logica do xadrez p/ logica da matriz

--- Program.cs
+++ Program.cs
@@ -8,21 +8,10 @@ namespace Chess_Curso
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(9, 0));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
         }
     }
 }
