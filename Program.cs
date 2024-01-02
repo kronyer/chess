937fcfb40f2d8d8e990acf15534af950fe212bdb
kronyer <pedromila2000@gmail.com>
2024-01-02 12:29:55 -0300

    Cores para as peças

--- Program.cs
+++ Program.cs
@@ -8,10 +8,21 @@ namespace Chess_Curso
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosicao());
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(5, 0));
+
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- Tela.cs
+++ Tela.cs
@@ -9,14 +9,31 @@ namespace Chess_Curso
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++) 
                 {
-                    if (tab.Peca(i,j) == null)
+                    if (tab.Peca(i, j) == null)
                         Console.Write("- ");
                     else
-                    Console.Write(tab.Peca(i,j) + " ");
+                        ImprimirPeca(tab.Peca(i, j));
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.WriteLine(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
